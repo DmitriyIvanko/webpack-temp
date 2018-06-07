0be8f816e981ece27305af6fbdceedf8372ade4d
DmitriyIvanko <ivanko.dmitriy@gmail.com>
2018-06-07 19:41:56 +0300

    + install dev-tool extention;

---
+++ src/app/app.effect.ts
@@ -0,0 +1,22 @@
+﻿import { Injectable } from "@angular/core";
+import { Action } from "@ngrx/store";
+import {
+    Actions,
+    Effect,
+    ofType,
+} from "@ngrx/effects";
+import { Observable, of } from "rxjs";
+import { catchError, map, mergeMap } from "rxjs/operators";
+
+@Injectable()
+export class AppEffects {
+
+    constructor(private actions$: Actions) { }
+
+    // @Effect({ dispatch: false })
+    // login$ = this.actions$.pipe(
+    //     ofType('RESET'),
+    //     map(action => {
+    //         console.log("Effected");
+    //     }));
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 ﻿import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 import { AppComponent } from "./app.component";
 import { counterReducer } from "./counter";
+import { AppEffects } from "./app.effect";
 
 @NgModule({
     declarations: [
@@ -12,6 +15,8 @@ import { counterReducer } from "./counter";
     imports: [
         BrowserModule,
         StoreModule.forRoot({ count: counterReducer }),
+        EffectsModule.forRoot([AppEffects]),
+        StoreDevtoolsModule.instrument(),
     ],
     bootstrap: [
       AppComponent,  

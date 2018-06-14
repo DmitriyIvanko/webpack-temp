32c5849d1054b0878174c55c222502f5ff659fe0
DmitriyIvanko <ivanko.dmitriy@gmail.com>
2018-06-14 13:34:19 +0300

    + small fix for vendor bundle;

--- src/app/vendor.ts
+++ src/app/vendor.ts
@@ -8,6 +8,13 @@ import "@angular/platform-browser-dynamic";
 import "@angular/core";
 import "@angular/common";
 import "@angular/router";
+import "@angular/http";
+import "@angular/forms";
 
 // RxJS
 import "rxjs";
+
+// Angular libraries
+import "@ngrx/effects";
+import "@ngrx/store";
+import "@ngrx/store-devtools";
